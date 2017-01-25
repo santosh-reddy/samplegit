20817a29afc8c3aaae1fb9750bc3cd24bd218436
santosh-reddy <santoshreddy.pc@gmail.com>
2017-01-25 17:14:10 +0530

    santosh commit

--- samplegit/ViewController.m
+++ samplegit/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //https://github.com/santosh-reddy/samplegit.git
 }
 
 
